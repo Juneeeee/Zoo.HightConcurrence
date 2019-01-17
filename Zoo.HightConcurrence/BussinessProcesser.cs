11957a0c8d11520df7a4a1c27d653056dbf7bfab
Juneeeee <june.it@qq.com>
2019-01-17 12:57:46 +0800

    优化代码

--- Zoo.HightConcurrence/BussinessProcesser.cs
+++ Zoo.HightConcurrence/BussinessProcesser.cs
@@ -12,16 +12,25 @@ namespace HighConcurrence.App
             long storeLength = QueueManager.Instance.QueueLength();
             if (storeLength < limit)
             {
-                // 此处应考虑写入的值在当前进程中及时被消费掉，否则造成队列资源浪费
+                try
+                {
+                    QueueManager.Instance.AddQueue(data.No);
+                    Console.WriteLine($"[{DateTime.Now.ToString()}]加入数据[No:{data.No}]");
 
 
-                QueueManager.Instance.AddQueue(data.No);
-                Console.WriteLine($"[{DateTime.Now.ToString()}]加入数据[No:{data.No}]");
+                    //处理当前请求中数据业务 
+                    Thread.Sleep(100);
 
-                //处理当前请求中数据业务 
-                Thread.Sleep(100);
-                QueueManager.Instance.RemoveQueue(data.No);
-                Console.WriteLine($"[{DateTime.Now.ToString()}]已处理数据[No:{data.No}]");
+                }
+                catch 
+                {
+                    
+                }
+                finally
+                {
+                    QueueManager.Instance.RemoveQueue(data.No);
+                    Console.WriteLine($"[{DateTime.Now.ToString()}]已处理数据[No:{data.No}]");
+                }
             }
             else
                 Console.WriteLine($"[{DateTime.Now.ToString()}]超出限制，无法处理数据，[No:{data.No}]");
